31f7e666452244002068dff047e0d3447d8ab59a
Kunal_©oder <166412409+devkunal2812@users.noreply.github.com>
2026-03-31 23:02:38 +0530

    Enhance SkillCard to support mobile rendering
    Added mobile prop to render SkillCard as a pill chip for mobile view.

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,13 +1,28 @@
 import styles from './SkillCard.module.css'
 
-export default function SkillCard({ position, delay, floatDur, iconVariant, icon, title, sub }) {
+// mobile=true → renders a pill chip for the scroll row
+// mobile=false (default) → renders the absolute floating card
+export default function SkillCard({ position, delay, floatDur, iconVariant, icon, title, sub, mobile = false }) {
+  if (mobile) {
+    return (
+      <div className={`${styles.mobilePill}`}>
+        <span
+          className={`${styles.pillIcon} ${styles[iconVariant]}`}
+          dangerouslySetInnerHTML={{ __html: icon }}
+        />
+        <span className={styles.pillTitle}>{title}</span>
+      </div>
+    )
+  }
+
   return (
     <div
       className={`${styles.card} ${styles[position]}`}
       style={{ '--delay': delay, '--float-dur': floatDur }}
       data-card
     >
-      <div className={`${styles.icon} ${styles[iconVariant]}`}
+      <div
+        className={`${styles.icon} ${styles[iconVariant]}`}
         dangerouslySetInnerHTML={{ __html: icon }}
       />
       <div className={styles.info}>
